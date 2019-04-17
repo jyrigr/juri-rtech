2fbb7d74b1e9a9e157071487ebe41f249054b07d
Juri Gramann <juri.gramann@ut.ee>
2019-04-18 01:19:06 +0300

    Practical 7 fix

--- src/range_visualizer/src/sensor_filter.cpp
+++ src/range_visualizer/src/sensor_filter.cpp
@@ -3,40 +3,75 @@
 #include "sensor_msgs/Range.h"
 
 #include <sstream>
+#include <vector>
 #include <cstdlib>
 #include <math.h>
 
-void sensorFilter(const sensor_msgs::Range::ConstPtr& msg) 
+class SensorFilter
 {
-	std_msgs::Header header;
-  header.seq  = msg->header.seq;
-  header.frame_id = "sensor_filter";
+public:
+  SensorFilter()
+  {
+    //Topic you want to publish
+    pub_ = n_.advertise<sensor_msgs::Range>("/distance/filter", 1000);
 
-	sensor_msgs::Range rmsg;
+    //Topic you want to subscribe
+    sub_ = n_.subscribe("/distance/raw", 1000, &SensorFilter::callback, this);
     
-  rmsg.header = header;
-  rmsg.radiation_type = sensor_msgs::Range::ULTRASOUND;
+    //Initialize vector
+    range_values = std::vector<float>(10);
     
-	rmsg.field_of_view = msg->field_of_view;
-  rmsg.min_range = msg->min_range;
-  rmsg.max_range = msg->max_range;
+  }
 
-	rmsg.range = 1;
+  void callback(const sensor_msgs::Range::ConstPtr& input)
+  {
+    std_msgs::Header header;
+    header.seq  = input->header.seq;
+    header.frame_id = "sensor_filter";
 
-	pub.publish(rmsg);
-}
+    sensor_msgs::Range rmsg;
+        
+    rmsg.header = header;
+    rmsg.radiation_type = sensor_msgs::Range::ULTRASOUND;
+    
+    range_values.insert(range_values.begin(), input->range);
+    range_values.erase(range_values.end());
+    
+    float range_sum = 0;
+    for(std::vector<float>::iterator it = range_values.begin(); it != range_values.end(); ++it) {
+        range_sum += *it;
+    }
+    
+    rmsg.range = range_sum / range_values.size();
+    
+    rmsg.field_of_view = input->field_of_view;
+    rmsg.min_range = input->min_range;
+    rmsg.max_range = input->max_range;
 
+    pub_.publish(rmsg); 
+    
+    return;
+  }
+  
+
+private:
+  ros::NodeHandle n_; 
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
+  
+  std::vector<float> range_values;
+
+};//End of class SensorFilter
 
 int main(int argc, char **argv)
 {
+  //Initiate ROS
+  ros::init(argc, argv, "random_range_filter");
 
-  ros::init(argc, argv, "random_range");
-  ros::NodeHandle n;
+  //Create an object of class SensorFilter that will take care of everything
+  SensorFilter SFObject;
 
-  ros::Publisher pub = n.advertise<sensor_msgs::Range>("distance/filter", 1000);
-	ros::Subscriber sub = n.subscribe ("distance/raw", 1000, sensorFilter);
-
-	ros::spin();
+  ros::spin();
 
   return 0;
 }
